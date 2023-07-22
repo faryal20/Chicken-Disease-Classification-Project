9fe4f8c8b286660ba93a89fd3a779e6d1b898068
Faryal <faryaltahseen20@gmail.com>
2023-07-23 02:24:14 +0500

    data ingestion Completed

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Chicken-Disease-Classification-Project
+
+## Workflows
+
+1. update the config.yaml
+2. update params.yaml
+3.update the entity
+4.update the Configuration Manager in src config
+5.update the components
+6.update the pipeline
+7.update the main.py
+8.update the dvc.yaml
---
+++ main.py
@@ -0,0 +1,15 @@
+from ChickenDisease import logger
+from ChickenDisease.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<\n\nxxxxxxxxxx")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
